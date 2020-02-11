d7a04a926272ff3586cceeabd83a2cb0b8327b39
hssnsdsol <hssnsdsol@gmail.com>
2020-02-11 17:37:44 +0500

    initial commmit

---
+++ app/src/main/java/com/example/kotlinpractice/UI/settings/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinpractice.UI.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment() {
+   /* override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+      //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }*/
+}
